e91c209dc0c2c12a07981c543cddeaa8c4e15a2d
Y <pingyun0425@gmail.com>
2026-05-20 15:47:38 +0800

    feat: add php files for each page

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>MOGUMOGU</title>
+</head>
+<body>
+    <a href="status.php">
+        <button>pet status</button>
+    </a>
+    <br><br>
+    <a href="log.php">
+        <button>log meal</button>
+    </a>
+    <a href="streak.php">
+        <button>streak</button>
+    </a>
+    <a href="history.php">
+        <button>meal history</button>
+    </a>
+</body>
---
+++ public/log.php
@@ -0,0 +1,4 @@
+<?php
+    echo "log meal here<br>";
+    echo "ᓚᘏᗢ";
+?>

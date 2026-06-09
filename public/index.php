1bc9dfdb80fb173ba4355ad9e350b2c926f5227c
acesavvy101 <gracielladeronne07@gmail.com>
2026-06-09 22:42:25 +0800

    feat: add contributors page in homepage

--- public/index.php
+++ public/index.php
@@ -22,6 +22,10 @@ update_health($pdo);
 <body>
     <!-- HOMEPAGE -->
      <div class="homepageContainer">
+        <div id="contributorsPage">
+            <img id="contributorsIcon"src="/assets/images/contributors.png">
+            <p id="contributors">contributors</p>
+        </div>
         <div id="title"><h1>Mogu Mogu Meal Tracker!</h1></div>
         <img id="pet"src="/assets/images/duckpet.png">
         <a href="petstatus.php">
